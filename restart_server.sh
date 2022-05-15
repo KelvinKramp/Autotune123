ace44d44a158772bc5307e46a8f1853bf5cebd75
Kelvin Kramp <k.h.kramp@gmail.com>
2022-05-15 16:35:18 +0200

    update

---
+++ restart_server.sh
@@ -0,0 +1,9 @@
+sudo service nginx stop
+pkill gunicorn
+sudo systemctl stop app
+sudo systemctl daemon-reload
+sudo nginx -t
+sudo systemctl start app
+sudo systemctl enable app
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full'
